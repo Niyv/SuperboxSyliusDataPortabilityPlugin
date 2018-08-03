d59e0b2c48157665cf6134d423ecf9c22f497553
Sascha Sedar <sascha.sedar@superbox.de>
2018-08-03 10:10:33 +0200

    Initial commit

---
+++ src/SuperboxDataPortabilityPlugin.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Superbox\SyliusDataPortabilityPlugin;
+
+use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class SuperboxDataPortabilityPlugin extends Bundle
+{
+    use SyliusPluginTrait;
+}
